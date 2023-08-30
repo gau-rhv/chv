3543c07d39de6b3ced677faf09c559fad0117c60
⸸ ジグソー ⛧ <102450738+j1gs4w-exe@users.noreply.github.com>
2023-08-30 11:26:29 +0530

    Delete Calvin Klien directory

--- Calvin Klien/nav.js
+++
@@ -1,7 +0,0 @@
-function redirectToCollectionsPage(filter) {
-    const currentPageURL = window.location.href;
-    const collectionsPageURL = "collections.html";
-    const url = new URL(collectionsPageURL, currentPageURL);
-    url.searchParams.set("filter", filter);
-    window.location.href = url.toString();
-}
